f92b833467e669677b23d5c2d5874a303a450ddc
aqueebjaved61 <102209554+aqueebjaved61@users.noreply.github.com>
2023-05-27 12:23:20 +0530

    Update sharp_db.sql

--- database file/sharp_db.sql
+++ database file/sharp_db.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.5.2
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Mar 15, 2023 at 04:21 PM
--- Server version: 5.7.9
--- PHP Version: 5.6.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
