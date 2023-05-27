e00befd30b239fd905038cd4a3b08db6e05f0282
aqueebjaved61 <102209554+aqueebjaved61@users.noreply.github.com>
2023-05-27 12:19:56 +0530

    Update pagination.php

--- phpclasses/pagination.php
+++ phpclasses/pagination.php
@@ -1,15 +1,5 @@
 <?php
-/**
- * CodexWorld is a programming blog. Our mission is to provide the best online resources on programming and web development.
- *
- * This Pagination class helps to integrate ajax pagination in PHP.
- *
- * @class		Pagination
- * @author		CodexWorld
- * @link		http://www.codexworld.com
- * @contact		http://www.codexworld.com/contact-us
- * @version		1.0
- */
+
 class Pagination{
 	var $baseURL		= '';
 	var $totalRows  	= '';
